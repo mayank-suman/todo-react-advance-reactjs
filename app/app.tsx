6abab843a33b65792ab5e38713ec8a59c654a529
Mayank Suman <mayankcsmn@gmail.com>
2025-01-20 18:32:11 +0000

    add delete reducer

--- app/app.tsx
+++ app/app.tsx
@@ -8,19 +8,21 @@ function App() {
   // const [tasks, setTasks] = useState<Task[]>([]);
   const reducer = (state: Task[], action: { type: string; payload: any }) => {
     switch (action.type) {
-      case "add_task":
+      case "add":
         return [
           ...state,
           { id: Date.now(), text: action.payload, completed: false },
         ];
+      case "delete":
+        return state.filter((task) => task.id != action.payload);
       default:
         return state;
     }
   };
-  const [tasks, dispatch] = useReducer(reducer, []);
 
-  const addTask = (text: string) =>
-    dispatch({ type: "add_task", payload: text });
+  const [tasks, dispatch] = useReducer(reducer, []);
+  const addTask = (text: string) => dispatch({ type: "add", payload: text });
+  const deleteTask = (id: number) => dispatch({ type: "delete", payload: id });
 
   return (
     <div>
